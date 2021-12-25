a7c38ca8fc8c1690e319c80825dfafd5ebc42713
Bharathwaj <41534996+bharathwajist@users.noreply.github.com>
2021-12-25 08:13:13 +0530

    Deleting desired elements from an array

---
+++ deleteFromArray.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void main(){
+    int p[100],i,n,a;
+    printf("Enter the length of the array : ");
+    scanf("%d",&n);
+    printf("Enter %d elements of the array \n",n);
+    for(i=0;i<=n-1;i++)
+        scanf("%d",&p[i]);
+    printf("\nThe array is:\n");
+    for(i=0; i <=n-1; i++)
+        printf("%d\n",p[i]);
+    printf("Enter the position/location to delete : ");
+    scanf("%d",&a);
+    a--;
+    for(i=a;i<=n-2;i++){
+        p[i]=p[i+1];
+        /* All values from the bottom of the array are shifted up
+         till the location of the element to be deleted */
+    }
+    p[n-1]=0;
+    // the vacant position created at the bottom of the array is set to 0
+    printf("Array after deleting the element is \n");
+    for(i=0;i<=n-2;i++)
+        printf("%d\n",p[i]);
+}
